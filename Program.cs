8950b7ff85337ad3a552f7eda991f779854f07d9
edwcmiller3 <edwcmiller3@outlook.com>
2021-02-15 12:32:36 -0500

    folder restructuring

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using DSharpPlus;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SaltBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        public class AppToken
+        {
+            public string Token { get; set; }
+        }
+        static async Task MainAsync()
+        {
+            string token = File.ReadAllText(@"..\..\..\token.txt");
+
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = token,
+                TokenType = TokenType.Bot
+            });
+
+            discord.MessageCreated += async e =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("ping"))
+                    await e.Message.RespondAsync("pong!");
+            };
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+
+}
